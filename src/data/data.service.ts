3321bb4d059be95b1ba7b0bceb6beedeb0792c17
Saba Sakandelidze <101635263+sabasako@users.noreply.github.com>
2024-05-23 23:59:56 +0400

    ADD ARRAY

--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DataService {
-  private dataStore;
+  private dataStore = [];
 
   getData() {
     return this.dataStore;
